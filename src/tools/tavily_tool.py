b358d9a306b2f66f1052ccf60fb78edeb1c6eb31
Shilpi Chadokar <shilpichadokar0@gmail.com>
2026-02-23 00:51:50 +0530

    config files made

---
+++ src/tools/tavily_tool.py
@@ -0,0 +1,20 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
+from src.config.config import TAVILY_API_KEY
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+def tavily_search_tool(query: str) -> str:
+    """
+    Search the web using Tavily to get up-to-date travel information,
+    attractions, activities, tips, and local insights for a given query.
+    """
+    tavily_search = TavilySearchResults(
+        max_results=5,
+        topic="general",
+        tavily_api_key=TAVILY_API_KEY
+    )
+    return tavily_search.invoke({"query": query})
+
+
+logger.info("TAVILY TOOL ALL SET")
